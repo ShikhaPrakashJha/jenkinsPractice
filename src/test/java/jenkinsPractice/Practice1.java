c1e0a37d9004cf8af19fa3b61a7f2dc1c7ac273a
ShikhaPrakashJha <shikha.prakash1994@gmail.com>
2023-03-10 17:44:34 +0530

    fisrt commit

---
+++ src/test/java/jenkinsPractice/Practice1.java
@@ -0,0 +1,22 @@
+package jenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class Practice1 {
+
+	@Test
+	public void test1() {
+		System.out.println("test---1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("test---2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("test---3");
+	}
+	
+}
+	
---
+++ src/test/java/jenkinsPractice/Practice2.java
@@ -0,0 +1,19 @@
+package jenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	@Test
+	public void test4() {
+		System.out.println("test---4");
+	}
+	@Test
+	public void test5() {
+		System.out.println("test---5");
+	}
+	
+	@Test
+	public void test6() {
+		System.out.println("test---6");
+	}
+}
